d92e968dde0557f4ffab3661cab34a0e35120074
Hansaka Franando <hansakafranando7@gmail.com>
2026-03-14 00:11:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/WpNDqSrd/freepik-highcontrast-dark-hackerthemed-logo-design-for-a-h-3878.png",
     ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Dexer MD Is Alive Now😍*",
-    BOT_OWNER: '94779912589',  // Replace with the owner's phone number
+    BOT_OWNER: '94742053080',  // Replace with the owner's phone number
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDOhACLNdsWwkygGGPpj99-c8AMleYhre4",
 
 };
