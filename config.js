1daea8574a06870b79014fdd6271ea9b55b1b616
Hansaka Franando <hansakafranando7@gmail.com>
2026-03-13 22:52:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/WpNDqSrd/freepik-highcontrast-dark-hackerthemed-logo-design-for-a-h-3878.png",
+    ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Dexer MD Is Alive Now😍*",
+    BOT_OWNER: '94779912589',  // Replace with the owner's phone number
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDOhACLNdsWwkygGGPpj99-c8AMleYhre4",
+
+};
